a288fb21fdd330280df7d2191d8b26a136f13625
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2021-11-04 20:51:17 +0100

    2021-11-04T19:51:17Z
    Signed-off-by: Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>

--- Helm/add-repos.sh
+++ Helm/add-repos.sh
@@ -4,5 +4,6 @@ helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add cowboysysop https://cowboysysop.github.io/charts/
+helm repo add kuma https://kumahq.github.io/charts
 
 helm repo update
